a859394b2e2fb136b844e3c3b75fc7a80e4a5e53
john137924865 <96287823+john137924865@users.noreply.github.com>
2025-12-20 01:05:02 +0100

    test swa

--- api/function_app.py
+++ api/function_app.py
@@ -5,54 +5,41 @@ from azure.cosmos import CosmosClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-# Variabili caricate da Azure Static Web App Settings
-COSMOS_CONN_STR = os.environ.get("CosmosDBConnectionString")
-DB_NAME = os.environ.get("DB_NAME")
-CONTAINER_NAME = os.environ.get("CONTAINER_NAME")
+def get_container():
+    conn_str = os.environ.get("CosmosDBConnectionString")
+    db_name = os.environ.get("DB_NAME")
+    container_name = os.environ.get("DB_CONTAINER_NAME")
+    
+    if not all([conn_str, db_name, container_name]):
+        raise ValueError("Mancano variabili d'ambiente!")
+        
+    client = CosmosClient.from_connection_string(conn_str)
+    return client.get_database_client(db_name).get_container_client(container_name)
 
-# Inizializzazione client Cosmos
-client = CosmosClient.from_connection_string(COSMOS_CONN_STR)
-database = client.get_database_client(DB_NAME)
-container = database.get_container_client(CONTAINER_NAME)
-
-@app.route(route="GetDevices")
+@app.route(route="GetDevices", methods=["GET"])
 def get_devices(req: func.HttpRequest) -> func.HttpResponse:
-    # Query per ottenere gli ID dei dispositivi senza duplicati
-    query = "SELECT DISTINCT c.deviceId FROM c"
-    items = list(container.query_items(query=query, enable_cross_partition_query=True))
-    devices = [item['deviceId'] for item in items]
-    
-    return func.HttpResponse(
-        json.dumps(devices),
-        mimetype="application/json",
-        status_code=200
-    )
+    try:
+        container = get_container()
+        query = "SELECT DISTINCT c.deviceId FROM c"
+        items = list(container.query_items(query=query, enable_cross_partition_query=True))
+        devices = [item['deviceId'] for item in items]
+        return func.HttpResponse(json.dumps(devices), mimetype="application/json", status_code=200)
+    except Exception as e:
+        return func.HttpResponse(json.dumps({"error": str(e)}), status_code=500, mimetype="application/json")
 
-@app.route(route="GetDeviceStats")
+@app.route(route="GetDeviceStats", methods=["GET"])
 def get_device_stats(req: func.HttpRequest) -> func.HttpResponse:
-    device_id = req.params.get('deviceId')
-    
-    if not device_id:
-        return func.HttpResponse("Parametro deviceId mancante", status_code=400)
+    try:
+        device_id = req.params.get('deviceId')
+        if not device_id:
+            return func.HttpResponse("Parametro deviceId mancante", status_code=400)
 
-    # Query SQL
-    query = """
-    SELECT 
-        AVG(c.temperature) as avgTemp, 
-        AVG(c.humidity) as avgHum, 
-        MIN(c.timestamp) as minTS, 
-        MAX(c.timestamp) as maxTS 
-    FROM c WHERE c.deviceId = @devId
-    """
-    params = [{"name": "@devId", "value": device_id}]
-    
-    items = list(container.query_items(query=query, parameters=params, enable_cross_partition_query=True))
-    
-    # Se il device esiste restituisce l'unico risultato
-    result = items[0] if items else {}
-    
-    return func.HttpResponse(
-        json.dumps(result),
-        mimetype="application/json",
-        status_code=200
-    )
+        container = get_container()
+        query = "SELECT AVG(c.temperature) as avgTemp, AVG(c.humidity) as avgHum FROM c WHERE c.deviceId = @devId"
+        params = [{"name": "@devId", "value": device_id}]
+        items = list(container.query_items(query=query, parameters=params, enable_cross_partition_query=True))
+        
+        result = items[0] if items else {}
+        return func.HttpResponse(json.dumps(result), mimetype="application/json")
+    except Exception as e:
+        return func.HttpResponse(json.dumps({"error": str(e)}), status_code=500)
